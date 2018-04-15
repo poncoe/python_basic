4b4f3b2a9875070b878fb9da0613d4ca5d9b5c9b
Kucing Item Putih <poncoe4mac@gmail.com>
2018-04-15 22:09:28 +0700

    added bab10 tp

---
+++ bab10_matplotlib/tp/modul10_dot.py
@@ -0,0 +1,10 @@
+import matplotlib.pyplot as plt
+
+x = [1,2,4,5,7]
+y = [1,3,5,4,6]
+
+plt.plot(x,y, "ro")
+plt.xlabel("x")
+plt.ylabel("y")
+plt.title("Matplotlib Modul 10 by Kucing Item Putih ")
+plt.show()
---
+++ bab10_matplotlib/tp/modul10_line.py
@@ -0,0 +1,11 @@
+import matplotlib.pyplot as plt
+
+x = [1,2,4,5,7]
+y = [1,3,5,4,6]
+
+plt.plot(x,y, label="")
+plt.xlabel("x")
+plt.ylabel("y")
+plt.title("Matplotlib Modul 10 by Kucing Item Putih ")
+plt.legend()
+plt.show()
