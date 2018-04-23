3466ac5d37cc1b822db6f5856d1f2a30e8c34dc6
Kucing Item Putih <poncoe4mac@gmail.com>
2018-04-24 00:43:27 +0700

    added test_skill in bab 9

---
+++ bab9_funcrecursive/test_skill/fibbonaci.py
@@ -0,0 +1,13 @@
+def fibbonaci(x):
+    if x == 0:
+        return 0
+    elif x == 1:
+        return 1
+    else:
+        return fibbonaci(x - 1) + fibbonaci(x - 2)
+
+
+if __name__ == '__main__':
+    k = int(input("Enter the number : "))
+    for i in range(k + 1):
+        print(fibbonaci(i), end=" ")
