6966602c18355dc9ff68694ce5f8b9061d246387
poncoe <poncoe4mac@gmail.com>
2019-03-15 23:15:22 +0700

    add justlearning

---
+++ justlearning/ListTuple.py
@@ -0,0 +1,39 @@
+# Operator (oprator)
+
+a = 2
+b = 4
+
+# Oprator +
+
+hasiltambah = a + b
+print(hasiltambah)
+
+# Oprator -
+
+hasilkurang = a - b
+print(hasilkurang)
+
+# Oprator *
+
+hasilperkalian = a * b
+print(hasilperkalian)
+
+# Oprator **
+
+hasilpangkat = a ** b
+print(hasilpangkat)
+
+# Oprator /
+
+hasilpembagian = a / b
+print(hasilpembagian)
+
+# Oprator // (div)
+
+hasildiv = a // b
+print(hasildiv)
+
+# Oprator % (Mod)
+
+hasilmod = a % b
+print(hasilmod)
---
+++ justlearning/Listtuple_part2.py
@@ -0,0 +1,18 @@
+# LIST
+
+##L1 = [62,13,73,13,235,1231,1,24,6]
+####L1.insert(3,60)
+####L2 = [7,8,9,10]
+####L1.extend(L2)
+####L.append(7)
+####L.append('Poncoe')
+####L1.reverse()
+##L1.sort()
+##print(L1)
+
+# TUPLE
+
+TA = (1,2,3,4,5,6)
+TB = ('a','b','c')
+TC = TA + TB
+print(TC)
