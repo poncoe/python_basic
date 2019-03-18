c042355396898fcb7b7edbd320bb94209aa5fc89
poncoe <poncoe4mac@gmail.com>
2019-03-18 22:43:24 +0700

    add justlearning

---
+++ justlearning/looping.py
@@ -0,0 +1,16 @@
+## WHILE
+
+##n = int(input("Masukan Angka = "))
+##i = 1
+##
+##while(i<=n):
+##    print(i)
+##    i = i+1
+
+## ==============
+
+# FOR
+
+list_hari = ['Senin', 'Selasa', 'Rabu', 'Kamis', 'Jumat']
+for tampil_hari in list_hari:
+    print("Hari ini adalah hari =",tampil_hari)
